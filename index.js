5e7ac9bde1bb21ea28896174777e7fe19e7cdd20
felix-717 <vaghs622@gmail.com>
2025-01-23 11:52:55 -0700

    applies stream implementation

--- index.js
+++ index.js
@@ -15,19 +15,14 @@ userInterface.on("line",async (input) => {
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const result = await model.generateContent(input);
-console.log(result.response.text());
+const result = await model.generateContentStream([input]);
 
-userInterface.prompt();
+for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    console.log(chunkText);
+}
+
+// console.log(result.response.text());
+
+// userInterface.prompt();
 })
-// async function main() {
-//   const genAI = new GoogleGenerativeAI("AIzaSyAinr9sp2nvFKw-_YWnuhba4cgdoXFHDHg");
-//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-//   const prompt = "Explain how AI works";
-
-//   const result = await model.generateContent(prompt);
-//   console.log(result.response.text());
-// }
-
-// main().catch(console.error);
