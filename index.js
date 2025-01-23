f548d61e4015e1beca8bb590729afc074e494f3f
felix-717 <vaghs622@gmail.com>
2025-01-23 11:46:53 -0700

    fixes dotenv issues and adds user interface

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const readline = require("readline")
+const dotenv = require("dotenv")
+dotenv.config()
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-async function main() {
-  const genAI = new GoogleGenerativeAI("AIzaSyAinr9sp2nvFKw-_YWnuhba4cgdoXFHDHg");
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const prompt = "Explain how AI works";
+userInterface.prompt()
 
-  const result = await model.generateContent(prompt);
-  console.log(result.response.text());
-}
+userInterface.on("line",async (input) => {
+// do main
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-main().catch(console.error);
+const result = await model.generateContent(input);
+console.log(result.response.text());
+
+userInterface.prompt();
+})
+// async function main() {
+//   const genAI = new GoogleGenerativeAI("AIzaSyAinr9sp2nvFKw-_YWnuhba4cgdoXFHDHg");
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+//   const prompt = "Explain how AI works";
+
+//   const result = await model.generateContent(prompt);
+//   console.log(result.response.text());
+// }
+
+// main().catch(console.error);
